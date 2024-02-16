7830701db170300f2d0bc509b118ded1aebb39a6
Pavel.818 <Pavel.818@yandex.ru>
2024-02-15 21:51:19 -0800

    Initial Commit

---
+++ MyStuff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStuff
+//
+//  Created by Pavel Абрамов on 2024-02-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
